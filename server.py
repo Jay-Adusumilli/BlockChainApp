99bd6193d37edf35f5305bb1d266f01755620982
jcnoe <josephcnoe@gmail.com>
2023-04-20 11:43:31 -0400

    Added new client and server programs. Server saves incoming client connection and messages into a dictionary. The client's key will be the message saved in the dictionary

--- server.py
+++ server.py
@@ -2,14 +2,14 @@ import socket
 from threading import Thread
 
 # server's IP address
-SERVER_HOST = "0.0.0.0"
+SERVER_HOST = socket.gethostname()
 SERVER_PORT = 5002 # port we want to use
 separator_token = "<SEP>" # we will use this to separate the client name & message
 
 # initialize list/set of all connected client's sockets
 client_sockets = set()
 # create a TCP socket
-s = socket.socket()
+s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 # make the port as reusable port
 s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 # bind the socket to the address we specified
