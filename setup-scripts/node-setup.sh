bb5b04c5372cb611e320acd29b9cb6eea07ae08a
dtothej247 <dtothej24.7@gmail.com>
2023-04-27 14:58:56 -0400

    Update node-setup.sh

--- setup-scripts/node-setup.sh
+++ setup-scripts/node-setup.sh
@@ -3,3 +3,17 @@
 # test
 
 echo "Test"
+
+# 1. Download Python, PyCryptodome
+sudo apt sudo apt-get update
+sudo apt-get install python3.8 python3-pip -y
+
+pip install pycryptodome -y
+
+
+# 2. Clone repository
+git clone https://github.com/Jay-Adusumilli/BlockChainApp & EPID=$!
+wait $EPID
+
+# 3. Starts the Client.py
+python client.py
