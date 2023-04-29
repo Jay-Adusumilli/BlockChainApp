02928d274562fb001dd446c053e9f1284baca036
jcnoe <josephcnoe@gmail.com>
2023-04-29 14:11:41 -0400

    gitignore, fixed print statement issues in client

--- client.py
+++ client.py
@@ -15,11 +15,11 @@ def send_setup_message(username, public_key):
         s.connect(('localhost', 4400))
         s.sendall(json.dumps(message).encode())
         response = s.recv(1024).decode()
-        print response
+        print(response)
         s.close()
         return True
     except Exception as e:
-        print "Error connecting to server:", e
+        print("Error connecting to server:", e)
         return False
 
 if len(sys.argv) == 2:
@@ -32,12 +32,12 @@ if len(sys.argv) == 2:
         with open('username.txt', 'w') as f:
             f.write(username)
         if send_setup_message(username, public_key):
-            print "Setup successful"
+            print("Setup successful")
         else:
-            print "Setup failed"
+            print("Setup failed")
 
     message = sys.argv[1]
-    print "Sending message:", message, "as", username
+    print("Sending message:", message, "as", username)
     # TODO: send forward message to server with the given message
 elif len(sys.argv) == 3 and sys.argv[1] == "directory":
     # TODO: send directory message to server and print the response
@@ -52,12 +52,12 @@ else:
         with open('username.txt', 'w') as f:
             f.write(username)
         if send_setup_message(username, public_key):
-            print "Setup successful"
+            print("Setup successful")
         else:
-            print "Setup failed"
+            print("Setup failed")
 
-    print "Username:", username
-    print "Public key:", public_key
+    print("Username:", username)
+    print("Public key:", public_key)
 
 def send_directory_message():
     message = {
@@ -68,14 +68,14 @@ def send_directory_message():
     try:
         s.connect(('localhost', 4400))
     except Exception as e:
-        print "Error connecting to server:", e
+        print("Error connecting to server:", e)
         return False
 
     message_str = json.dumps(message)
     s.sendall(message_str.encode())
 
     response = s.recv(1024).decode()
-    print "Server response:", response
+    print("Server response:", response)
     s.close()
     return True
 
@@ -91,28 +91,28 @@ def send_forward_message(username, message):
     try:
         s.connect(('localhost', 4400))
     except Exception as e:
-        print "Error connecting to server:", e
+        print("Error connecting to server:", e)
         return False
 
     message_str = json.dumps(message)
     s.sendall(message_str.encode())
 
     response = s.recv(1024).decode()
-    print "Server response:", response
+    print("Server response:", response)
     s.close()
     return True
 
 
 if __name__ == '__main__':
     if len(sys.argv) < 2:
-        print "Usage: python client.py <message>"
+        print("Usage: python client.py <message>")
         sys.exit()
 
     username = "user" + str(random.randint(1, 100))
     public_key = random.randint(1, 100)
 
-    print "Username:", username
-    print "Public key:", public_key
+    print("Username:", username)
+    print("Public key:", public_key)
 
     message_type = sys.argv[1]
 
@@ -132,5 +132,5 @@ if __name__ == '__main__':
         message = " ".join(sys.argv[2:])
         send_forward_message(username, message)
     else:
-        print "Invalid message type. Valid message types are 'setup', 'directory', and 'forward'"
+        print("Invalid message type. Valid message types are 'setup', 'directory', and 'forward'")
         sys.exit()
